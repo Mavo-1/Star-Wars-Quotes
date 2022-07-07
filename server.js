98a65a8af74f32f6f32cacbbca8a8f0f5ceacca3
Mavo-1 <103069153+Mavo-1@users.noreply.github.com>
2022-07-06 23:51:49 -0500

    Add files via upload

--- server.js
+++ server.js
@@ -1,19 +1,38 @@
+
 const express = require('express');
 const bodyParser= require('body-parser')
 const MongoClient= require('mongodb').MongoClient
-MongoClient.connect('mongodb+srv://Yoda:Mavericks1@cluster0.ewzcx1v.mongodb.net/?retryWrites=true&w=majority',{useUnifiedTopology: true})
-.then(client => {
-    console.log('Connected to Database')
-    const db = client.db('star-wars-quotes')
-})
 const app= express();
-app.listen(3000,function(){
-    console.log('Jarvis now turning on server')
-})
+
+
+
+
+MongoClient.connect('mongodb+srv://Yoda:Mavericks1@cluster0.ewzcx1v.mongodb.net/?retryWrites=true&w=majority',{useUnifiedTopology: true })
+    .then(client=>{
+    console.log('Jarvis connecting to Database')
+    const db= client.db('star-wars-quotes')
+    const quotesCollection= db.collection('quotes')
+
+app.set('view engine','ejs')
 app.use(bodyParser.urlencoded({extended: true}))
-app.get('/',(req,res)=>{
-    res.sendFile('C:/Users/mavoh/OneDrive/Desktop/Star Wars Quote App'+ '/index.html')
-})
-app.post('/quotes',(req,res)=>{
-    console.log(req.body)
+app.get('/', (req, res) => {
+    db.collection('quotes').find().toArray()
+      .then(results=>{
+        res.render('index.ejs',{quotes:results})
+      })
+      .catch(/* ... */)
+    
+  })
+app.post('/quotes', (req, res) => {
+    quotesCollection.insertOne(req.body)
+    .then(result =>{
+        res.redirect('/')
+    })
+    .catch(error =>console.error(error))
+  })
+
+app.listen(3000, function() {
+    console.log('listening on 3000')
+  })
+    
 })
