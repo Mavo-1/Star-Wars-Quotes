f831965271a4eac2acde3a38890c792edccc9b7b
Mavo-1 <103069153+Mavo-1@users.noreply.github.com>
2022-07-15 14:26:08 -0500

    Add files via upload

--- server.js
+++ server.js
@@ -15,23 +15,58 @@ MongoClient.connect('mongodb+srv://Yoda:Mavericks1@cluster0.ewzcx1v.mongodb.net/
 
 app.set('view engine','ejs')
 app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
+app.use(express.static('public'))
 
 app.get('/', (req, res) => {
-    db.collection('quotes').find().toArray()
-      .then(results=>{
-        res.render('index.ejs',{quotes:results})
-      })
-      .catch(/* ... */)
-    
-  })
-app.post('/quotes', (req, res) => {
-    quotesCollection.insertOne(req.body)
-    .then(result =>{
-        res.redirect('/')
+  db.collection('quotes').find().toArray()
+    .then(result=>{
+      res.render('index.ejs',{quotes:result})
     })
-    .catch(error =>console.error(error))
+    .catch(/* ... */)
+  
+})
+app.post('/quotes', (req, res) => {
+  quotesCollection.insertOne(req.body)
+  .then(result =>{
+      res.redirect('/')
   })
+  .catch(error =>console.error(error))
+})
 
+
+  
+  app.put('/quotes', (req, res) => {
+    quotesCollection.findOneAndUpdate(
+      { name: 'Yoda' },
+  {
+    $set: {
+      name: req.body.name,
+      quote: req.body.quote
+    }
+  },
+  {
+    upsert: true
+  }
+)
+  .then(result => {
+    res.json('Success')
+  })
+  .catch(error => console.error(error))
+  })
+app.delete('/quotes',(req,res) =>{
+  quotesCollection.deleteOne(
+    {name:req.body.name},
+    
+  )
+  .then(result =>{
+    if (result.deletedCount === 0){
+      return res.json('No quote to delete')
+    }
+    res.json("Deleted Darth Vader's quote")
+  })
+  .catch(error =>console.error(error))
+})
 app.listen(3000, function() {
     console.log('listening on 3000')
   })
